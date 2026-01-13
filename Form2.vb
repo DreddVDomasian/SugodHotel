3f30e2ab8ade9b839124b45b26ce551106bb64ed
lorence <gtc.johnlorence.maguate@cvsu.edu.ph>
2026-01-14 00:11:13 +0800

    .

--- Form2.vb
+++ Form2.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
 End Class
