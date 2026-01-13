b4ba346bb93bdfd15f6269e5778542a7102addf2
FrancisJerusalem <181956748+JohnnyCodeJourney@users.noreply.github.com>
2026-01-14 00:56:27 +0800

    d
    sda

--- GuestManagement.Designer.vb
+++ GuestManagement.Designer.vb
@@ -22,16 +22,115 @@ Partial Class GuestManagement
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button7)
+        Me.GroupBox1.Controls.Add(Me.Button6)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 29)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(229, 491)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(210, 37)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "DASHBOARD"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 84)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(210, 37)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "ROOM MANAGEMENT"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 139)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(210, 37)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "GUEST MANAGEMENT"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(6, 208)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(210, 37)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "CHECK-IN"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 264)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(210, 37)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "CHECK-OUT"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 307)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(210, 37)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "REPORTS"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(6, 376)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(210, 37)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "STAFF MANAGEMENT"
+        Me.Button7.UseVisualStyleBackColor = True
         '
         'GuestManagement
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1343, 592)
+        Me.ClientSize = New System.Drawing.Size(1305, 631)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "GuestManagement"
         Me.Text = "GuestManagement"
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
 End Class
