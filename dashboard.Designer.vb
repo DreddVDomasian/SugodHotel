3f1d460d8fe1caab8b602740d17ea931af1139ba
rmmln <rmmoleno1@gmail.com>
2026-01-14 00:13:34 +0800

    .

--- dashboard.Designer.vb
+++ dashboard.Designer.vb
@@ -22,16 +22,52 @@ Partial Class dashboard
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        '
-        'dashboard
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1024, 587)
-        Me.Name = "dashboard"
-        Me.Text = "dashboard"
-        Me.ResumeLayout(False)
+		Me.components = New System.ComponentModel.Container()
+		Me.Panel1 = New System.Windows.Forms.Panel()
+		Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.Panel1.SuspendLayout()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'Panel1
+		'
+		Me.Panel1.Controls.Add(Me.PictureBox1)
+		Me.Panel1.Location = New System.Drawing.Point(12, 12)
+		Me.Panel1.Name = "Panel1"
+		Me.Panel1.Size = New System.Drawing.Size(1037, 89)
+		Me.Panel1.TabIndex = 0
+		'
+		'NotifyIcon1
+		'
+		Me.NotifyIcon1.Text = "NotifyIcon1"
+		Me.NotifyIcon1.Visible = True
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Image = Global.SugodHotel.My.Resources.Resources.cutLogo
+		Me.PictureBox1.Location = New System.Drawing.Point(24, 27)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(66, 41)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox1.TabIndex = 0
+		Me.PictureBox1.TabStop = False
+		'
+		'dashboard
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(1063, 644)
+		Me.Controls.Add(Me.Panel1)
+		Me.Name = "dashboard"
+		Me.Text = " "
+		Me.Panel1.ResumeLayout(False)
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
 
-    End Sub
+	End Sub
+
+	Friend WithEvents Panel1 As Panel
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+	Friend WithEvents PictureBox1 As PictureBox
 End Class
