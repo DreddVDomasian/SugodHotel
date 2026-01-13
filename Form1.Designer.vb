56963b168bb13c50df0a509b1bd7e58023323c26
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2026-01-13 22:42:38 +0800

    login ui

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,38 +24,295 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label11 = New System.Windows.Forms.Label()
+        Me.label9 = New System.Windows.Forms.Label()
+        Me.label8 = New System.Windows.Forms.Label()
+        Me.label10 = New System.Windows.Forms.Label()
+        Me.label7 = New System.Windows.Forms.Label()
+        Me.label6 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.loginBtn = New System.Windows.Forms.Button()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.label14 = New System.Windows.Forms.Label()
+        Me.label13 = New System.Windows.Forms.Label()
+        Me.usernameBox = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(189, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(34, Byte), Integer))
-        Me.Panel1.Location = New System.Drawing.Point(135, 41)
+        Me.Panel1.Controls.Add(Me.label11)
+        Me.Panel1.Controls.Add(Me.label9)
+        Me.Panel1.Controls.Add(Me.label8)
+        Me.Panel1.Controls.Add(Me.label10)
+        Me.Panel1.Controls.Add(Me.label7)
+        Me.Panel1.Controls.Add(Me.label6)
+        Me.Panel1.Controls.Add(Me.label5)
+        Me.Panel1.Controls.Add(Me.label4)
+        Me.Panel1.Controls.Add(Me.label3)
+        Me.Panel1.Controls.Add(Me.label2)
+        Me.Panel1.Controls.Add(Me.label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(111, 41)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(364, 461)
+        Me.Panel1.Size = New System.Drawing.Size(400, 509)
         Me.Panel1.TabIndex = 0
         '
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.loginBtn)
+        Me.Panel2.Controls.Add(Me.PasswordBox)
+        Me.Panel2.Controls.Add(Me.label14)
+        Me.Panel2.Controls.Add(Me.label13)
+        Me.Panel2.Controls.Add(Me.usernameBox)
+        Me.Panel2.Controls.Add(Me.Label12)
         Me.Panel2.Location = New System.Drawing.Point(505, 41)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(364, 461)
+        Me.Panel2.Size = New System.Drawing.Size(404, 509)
         Me.Panel2.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.SugodHotel.My.Resources.Resources.cutLogo
+        Me.PictureBox1.Location = New System.Drawing.Point(128, 40)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(110, 110)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Montserrat SemiBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label1.Location = New System.Drawing.Point(81, 153)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(217, 30)
+        Me.label1.TabIndex = 2
+        Me.label1.Text = "LUXURY && COMFORT"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Montserrat Black", 24.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label2.Location = New System.Drawing.Point(51, 183)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(279, 52)
+        Me.label2.TabIndex = 3
+        Me.label2.Text = "SUGOD HOTEL"
+        '
+        'label11
+        '
+        Me.label11.AutoSize = True
+        Me.label11.Font = New System.Drawing.Font("Montserrat ExtraBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label11.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label11.Location = New System.Drawing.Point(113, 456)
+        Me.label11.Name = "label11"
+        Me.label11.Size = New System.Drawing.Size(137, 25)
+        Me.label11.TabIndex = 20
+        Me.label11.Text = "RYAN ORTEGA"
+        '
+        'label9
+        '
+        Me.label9.AutoSize = True
+        Me.label9.Font = New System.Drawing.Font("Montserrat SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label9.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.label9.Location = New System.Drawing.Point(119, 376)
+        Me.label9.Name = "label9"
+        Me.label9.Size = New System.Drawing.Size(134, 25)
+        Me.label9.TabIndex = 18
+        Me.label9.Text = "Robert Moleno"
+        '
+        'label8
+        '
+        Me.label8.AutoSize = True
+        Me.label8.Font = New System.Drawing.Font("Montserrat SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label8.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.label8.Location = New System.Drawing.Point(104, 351)
+        Me.label8.Name = "label8"
+        Me.label8.Size = New System.Drawing.Size(162, 25)
+        Me.label8.TabIndex = 17
+        Me.label8.Text = "Piel Paolo Cabebe"
+        '
+        'label10
+        '
+        Me.label10.AutoSize = True
+        Me.label10.Font = New System.Drawing.Font("Montserrat SemiBold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label10.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label10.Location = New System.Drawing.Point(67, 432)
+        Me.label10.Name = "label10"
+        Me.label10.Size = New System.Drawing.Size(231, 24)
+        Me.label10.TabIndex = 19
+        Me.label10.Text = "DEDICATED TO OUR FRIEND"
+        '
+        'label7
+        '
+        Me.label7.AutoSize = True
+        Me.label7.Font = New System.Drawing.Font("Montserrat SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label7.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.label7.Location = New System.Drawing.Point(85, 326)
+        Me.label7.Name = "label7"
+        Me.label7.Size = New System.Drawing.Size(201, 25)
+        Me.label7.TabIndex = 16
+        Me.label7.Text = "John Lorence Maguate"
+        '
+        'label6
+        '
+        Me.label6.AutoSize = True
+        Me.label6.Font = New System.Drawing.Font("Montserrat SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label6.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.label6.Location = New System.Drawing.Point(81, 301)
+        Me.label6.Name = "label6"
+        Me.label6.Size = New System.Drawing.Size(205, 25)
+        Me.label6.TabIndex = 15
+        Me.label6.Text = "John Francis Jerusalem"
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Font = New System.Drawing.Font("Montserrat SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label5.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.label5.Location = New System.Drawing.Point(113, 276)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(134, 25)
+        Me.label5.TabIndex = 14
+        Me.label5.Text = "Gianne Balneg"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Font = New System.Drawing.Font("Montserrat SemiBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label4.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.label4.Location = New System.Drawing.Point(104, 251)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(152, 25)
+        Me.label4.TabIndex = 13
+        Me.label4.Text = "Dredd Domasian"
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Font = New System.Drawing.Font("Montserrat ExtraBold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label3.Location = New System.Drawing.Point(119, 226)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(128, 25)
+        Me.label3.TabIndex = 12
+        Me.label3.Text = "DEVELOPERS"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Montserrat Black", 24.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label12.Location = New System.Drawing.Point(76, 40)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(268, 52)
+        Me.Label12.TabIndex = 4
+        Me.Label12.Text = "ADMIN LOGIN"
+        '
+        'loginBtn
+        '
+        Me.loginBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(189, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.loginBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.loginBtn.Font = New System.Drawing.Font("Montserrat ExtraBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.loginBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.loginBtn.Location = New System.Drawing.Point(33, 432)
+        Me.loginBtn.Name = "loginBtn"
+        Me.loginBtn.Size = New System.Drawing.Size(343, 48)
+        Me.loginBtn.TabIndex = 19
+        Me.loginBtn.Text = "Log in"
+        Me.loginBtn.UseVisualStyleBackColor = False
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Font = New System.Drawing.Font("Montserrat SemiBold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PasswordBox.Location = New System.Drawing.Point(33, 318)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.PasswordBox.Size = New System.Drawing.Size(343, 33)
+        Me.PasswordBox.TabIndex = 18
+        '
+        'label14
+        '
+        Me.label14.AutoSize = True
+        Me.label14.Font = New System.Drawing.Font("Montserrat SemiBold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label14.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label14.Location = New System.Drawing.Point(29, 262)
+        Me.label14.Name = "label14"
+        Me.label14.Size = New System.Drawing.Size(86, 24)
+        Me.label14.TabIndex = 17
+        Me.label14.Text = "Password"
+        '
+        'label13
+        '
+        Me.label13.AutoSize = True
+        Me.label13.Font = New System.Drawing.Font("Montserrat SemiBold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label13.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.label13.Location = New System.Drawing.Point(32, 153)
+        Me.label13.Name = "label13"
+        Me.label13.Size = New System.Drawing.Size(89, 24)
+        Me.label13.TabIndex = 16
+        Me.label13.Text = "Username"
+        '
+        'usernameBox
+        '
+        Me.usernameBox.Font = New System.Drawing.Font("Montserrat SemiBold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.usernameBox.Location = New System.Drawing.Point(33, 202)
+        Me.usernameBox.Name = "usernameBox"
+        Me.usernameBox.Size = New System.Drawing.Size(343, 33)
+        Me.usernameBox.TabIndex = 15
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Silver
-        Me.ClientSize = New System.Drawing.Size(985, 561)
+        Me.ClientSize = New System.Drawing.Size(1024, 587)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Panel2 As Panel
+    Private WithEvents label2 As Label
+    Private WithEvents label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Private WithEvents label11 As Label
+    Private WithEvents label9 As Label
+    Private WithEvents label8 As Label
+    Private WithEvents label10 As Label
+    Private WithEvents label7 As Label
+    Private WithEvents label6 As Label
+    Private WithEvents label5 As Label
+    Private WithEvents label4 As Label
+    Private WithEvents label3 As Label
+    Private WithEvents Label12 As Label
+    Private WithEvents loginBtn As Button
+    Private WithEvents PasswordBox As TextBox
+    Private WithEvents label14 As Label
+    Private WithEvents label13 As Label
+    Private WithEvents usernameBox As TextBox
 End Class
