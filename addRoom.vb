1b7c805c1f5cf60f9f93c98c754714b2c3b174d8
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2026-01-14 00:52:57 +0800

    addroom ui

---
+++ addRoom.vb
@@ -0,0 +1,25 @@
+﻿Public Class addRoom
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub addRoomBtn_Click(sender As Object, e As EventArgs) Handles MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click, MyBaseBtn.Click
+
+    End Sub
+End Class
