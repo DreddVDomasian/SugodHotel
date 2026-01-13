5987d1baa275a875eda1e34be9901b213352407c
Dredd Villarba Domasian <Domasiandredd@gmail.com>
2026-01-13 22:57:51 +0800

    isapa

---
+++ Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports MySql.Data.MySqlClient
+Module Module1
+    Public con As New MySqlConnection
+    Public cmd As New MySqlCommand
+
+    Sub openCon()
+        con.ConnectionString = "server=localhost;userid=root;password=;database=sugodhotel"
+        con.Open()
+    End Sub
+End Module
+
